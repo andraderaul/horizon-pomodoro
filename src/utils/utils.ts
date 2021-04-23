8aab9f3fc649b0772ef4c319787bfd02cab8e5e8
Raul Andrade <theandraderaul@gmail.com>
2021-04-22 22:22:18 -0300

    :hammer: refac all components to typescript

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,9 @@
-const formatTime = (minutes, remainingSeconds) => {
+const formatTime = (minutes: number, remainingSeconds: number): string => {
   return `${minutes < 10 ? 0 : ""}${minutes}:${
     remainingSeconds < 10 ? 0 : ""
   }${remainingSeconds}`;
 };
-export const displayTimeLeft = (seconds) => {
+export const displayTimeLeft = (seconds: number): string => {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
   const formattedTime = formatTime(minutes, remainingSeconds);
